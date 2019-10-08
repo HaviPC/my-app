2cd13b2038e9d693622e5152e9adeba16f39d2b2
HaviPC <havi.pkodi@gmail.com>
2019-10-08 16:35:22 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-app';
+  title1 = 1;
+  value2:number=2;
+  value3:number;
+  ngOnInit(){
+    localStorage.setItem('whatever', 'title1');
+    localStorage.setItem('whatever2', JSON.stringify(this.value2));
+    console.log(localStorage.getItem('whatever'));
+    console.log(localStorage.getItem('whatever2'));
+    this.value3 =JSON.parse(localStorage.getItem('whatever2'));
+    console.log( this.value3);
+  }
+
+}
